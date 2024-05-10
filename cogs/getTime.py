0f6124e3c552b80445bac0bbf98eecca8cb3b758
Minh Dong <minhdongk@gmail.com>
2024-05-10 19:13:08 -0400

    Updated getTime.py to get the current
    Removed old functions and only implemented the datetime function to get the current time for the bot

--- cogs/getTime.py
+++ cogs/getTime.py
@@ -4,44 +4,7 @@
 
 import discord
 from discord.ext import commands
-from functools import wraps
-from time import perf_counter, sleep
-
-
-def get_time(func):
-    """Times any function"""
-
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        start_time = perf_counter()
-
-        func(*args, **kwargs)
-
-        end_time = perf_counter()
-
-        total_time = round(end_time - start_time, 2)
-
-        print('Time', total_time, 'seconds')
-
-    return wrapper
-
-@get_time
-def do_something(param: str):
-    """Does something important"""
-
-    sleep(1)
-    print(param)
-
-
-
-def even_number():
-    evens: list = []
-    for number in range(50):
-        is_even = number % 2 == 0
-        if is_even:
-            evens.append(number)
-
-    return evens
+from datetime import datetime
 
 
 class GetTime(commands.Cog):
@@ -50,11 +13,7 @@ class GetTime(commands.Cog):
 
     @commands.hybrid_command(name="gettime", description="Get Time")
     async def ping(self, ctx):
-        do_something('yay')
-        print(do_something.__name__)
-        print(do_something.__doc__)
-        print(even_number())
-        await ctx.send('Get time function in development')
+        await ctx.send(f"The current time for this bot is {datetime.now().strftime('%H:%M:%S')} EST")
 
 
 async def setup(bot: commands.Bot) -> None:
