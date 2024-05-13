20eed9383ad5d7a59131975fb6fc785c5884cc68
Minh Dong <minhdongk@gmail.com>
2024-05-12 20:14:29 -0400

    Fixed issue with getting full message
    The first argument was not getting the entire string and only the first text. Added a response when the bot receives a new message for response in the future

--- cogs/chat/selfLearningChat.py
+++ cogs/chat/selfLearningChat.py
@@ -7,6 +7,7 @@
 import discord
 from discord.ext import commands
 from chatbot import get_knowledge_base_filename, load_knowledge_base, save_knowledge_base, find_best_match, get_answer_for_questions
+import os
 
 
 class SelfLearningChat(commands.Cog):
@@ -14,16 +15,27 @@ class SelfLearningChat(commands.Cog):
         self.bot = bot
         self.filename = get_knowledge_base_filename()
 
+        # If the file does not exist, let's create it for the first time
+        if not os.path.exists(self.filename):
+            with open(self.filename, "w") as file:
+                file.write("{\n")
+                file.write("  \"questions\": [\n")
+                file.write("  ]\n")
+                file.write("}\n")
+
     @commands.hybrid_command(name="chat",
                              brief="Chat with me!",
                              description="Chat with me!")
     async def chat(self,
                    ctx: discord.ext.commands,
+                   *,
                    user_message: str,
-                   store_message: str = None,
-                   ) -> None:
+                   store_message: str = None) -> None:
         # Get the knowledge base
         knowledge_base: dict = load_knowledge_base(self.filename)
+
+        #print(ctx.message.content)
+        #print(user_message)
 
         if store_message is None:
             # Get the answer for the msg
@@ -39,6 +51,7 @@ class SelfLearningChat(commands.Cog):
         elif store_message is not None:
             knowledge_base["questions"].append({"question": user_message, "answer": store_message})
             save_knowledge_base(self.filename, knowledge_base)
+            await ctx.send("Thank you for teaching me how to respond back in the future! Database updated! :)")
 
 
 # To add the bot to the cogs list
