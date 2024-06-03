64ef519aa68991554a1b9c5e4deb5e8ee9aea06b
Minh Dong <minhdongk@gmail.com>
2024-06-03 13:00:07 -0400

    Added !claim and functionalities
    Added a new feature to allow users to do !claim and the next claim will be in 24 hours.

--- economy/models/account.py
+++ economy/models/account.py
@@ -1,16 +1,22 @@
+#
+# account.py is a class file for getting the account information. the fetch will fetch the user's data so info can be
+# manipulated like account's amount (for economy purposes), next claim for the next time the user can claim their
+# points, etc.
+#
 
 import peewee
 from economy import get_economy_database
+from datetime import datetime, timedelta
 
 
 class Account(peewee.Model):
     user_id: str = peewee.CharField(max_length=255)
     guild_id: str = peewee.CharField(max_length=255)
     amount: int = peewee.IntegerField()
+    next_claim: datetime = peewee.DateTimeField(default=datetime.now)
 
     class Meta:
         database = get_economy_database()
-
 
     @staticmethod
     def fetch(message):
@@ -20,5 +26,6 @@ class Account(peewee.Model):
         except peewee.DoesNotExist:
             account = Account.create(user_id=message.author.id,
                                      guild_id=message.guild.id,
-                                     amount=0)
+                                     amount=0,
+                                     next_claim=datetime.now() - timedelta(hours=24))
         return account
