491885a58edacb093cd791c07bffa52892777b91
Minh Dong <minhdongk@gmail.com>
2024-06-19 20:16:29 -0400

    Balance bug fix
    A bug fix where updating the account balance was going negative. Should now go to 0 instead into the negatives

--- economy/balance.py
+++ economy/balance.py
@@ -3,10 +3,17 @@ from economy.models.account import Account
 
 
 def updateAccountBalance(account, winner: bool, amount: int):
+    # the final amount to be used
+    final_amount: int = amount
+
+    # Make sure the account amount is never less than 0. If it is, just deduct the remaining points
+    if account.amount < amount:
+        final_amount = account.amount
+
     # Did the user win?
     if winner:
-        account.amount += amount
+        account.amount += final_amount
     else:
-        account.amount -= amount
+        account.amount -= final_amount
 
     account.save()
